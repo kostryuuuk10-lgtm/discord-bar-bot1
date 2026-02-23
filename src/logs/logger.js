e070baecee56c298c7471c05e22a16259d413a0d
kostryuuuk10-lgtm <kostryuuuk10@gmail.com>
2026-02-23 18:32:42 +0300

    Delete src directory

--- src/logs/logger.js
+++
@@ -1,6 +0,0 @@
-module.exports = async function log(client, text){
-  const { LOG_CHANNEL_ID } = require("../config/env");
-  if(!LOG_CHANNEL_ID) return;
-  const ch = await client.channels.fetch(LOG_CHANNEL_ID).catch(()=>null);
-  if(ch) ch.send("📜 " + text);
-};
